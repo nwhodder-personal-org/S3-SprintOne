42297cb48313aa63f67719b0c899ee7bdcaf9e58
Nicholas Hodder <Nicholas.Hodder@keyin.com>
2023-02-20 20:29:05 -0330

    Fleshed out Patron Class with some light testing

--- Main.java
+++ Main.java
@@ -7,10 +7,14 @@ public class Main {
         Author markTwain = new Author();
         markTwain.setName("Mark Twain");
         Book book = new Book("Huck Finn", markTwain, "EXAMPLEISBN001", "yesCorp", 1);
+        Book book2 = new Book("Smuch Smin", markTwain, "Examplbndaw", "yeye", 2);
         System.out.println(book);
         Patron nick = new Patron();
         nick.setName("Nick");
-        System.out.println(book.borrowBook(book, nick));
-        System.out.println(book.borrowBook(book, nick));
+        nick.addBook(book2);
+        nick.getBorrowedBooks();
+//        System.out.println(book.borrowBook(book, nick));
+//        System.out.println(book.borrowBook(book, nick));
+//        System.out.println(nick.removeBook(book));
     }
 }
--- classes/Patron.java
+++ classes/Patron.java
@@ -1,9 +1,16 @@
 package classes;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+
 public class Patron {
     private String name;
+    private LocalDate dateOfBirth;
+    private String address;
+    private ArrayList<Book> borrowedBooks;
 
     public Patron() {
+        this.borrowedBooks = new ArrayList<Book>();
     }
 
     public String getName() {
@@ -12,5 +19,41 @@ public class Patron {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(int year, int month, int day) {
+        this.dateOfBirth = LocalDate.of(year, month, day);
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public ArrayList<Book> getBorrowedBooks() {
+        System.out.printf("List of books borrowed by %s:%n", this.getName());
+        borrowedBooks.forEach((x) -> System.out.println("-" + x.getTitle()));
+        return borrowedBooks;
+    }
+
+    public void setBorrowedBooks(ArrayList<Book> borrowedBooks) {
+        this.borrowedBooks = borrowedBooks;
+    }
+
+    public ArrayList<Book> addBook(Book book){
+        this.borrowedBooks.add(book);
+        return this.borrowedBooks;
+    }
+
+    public ArrayList<Book> removeBook(Book book){
+        this.borrowedBooks.remove(book);
+        return this.borrowedBooks;
     }
 }
